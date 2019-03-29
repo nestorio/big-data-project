5042ca4e7fb5224f6637ceed8f3d669f1c828bb2
Ray Z <nestorcharly@yahoo.com>
2019-03-29 12:50:03 +0100

    The static folder contain contains the resources of the interface and the templates folder contain the templates. I have added some got to the __init__.py. Check in the comments

--- bigdata_app/code/__init__.py
+++ bigdata_app/code/__init__.py
@@ -1,1 +1,24 @@
- 
+from flask import Flask, render_template, url_for, request, session, redirect
+import pymongo
+import bcrypt
+app = Flask(__name__)
+
+#This is the route that leads to the index page
+@app.route('/')
+def index():
+    if('username' in session):
+        return "You are logged in as" + session['username']
+    return render_template('index.html')
+
+# This is the login route which links to the login page
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+# This is the registration route which lead to the registration page
+@app.route('/register')
+def register():
+    return render_template('sign-up.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
